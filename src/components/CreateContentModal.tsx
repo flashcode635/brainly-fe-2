c013d309dece7fb8bb443a794acdd32d2181141a
flashcode635 <151376451+flashcode635@users.noreply.github.com>
2025-10-23 23:07:43 +0530

    Refactor CreateContentModal background styling
    Updated background color opacity issue

--- src/components/CreateContentModal.tsx
+++ src/components/CreateContentModal.tsx
@@ -36,12 +36,13 @@ export function CreateContentModal({open, onClose}) {
 
     return <div>
         {open && <div> 
-            <div className="w-screen h-screen bg-slate-500 fixed top-0 left-0 opacity-60 flex justify-center">
+            {/* used lighter version of color with required opacity rather than using opacity property */}
+            <div className="w-screen h-screen bg-slate-500 fixed top-0 left-0 bg-[#191724f8] z-1000 flex justify-center">
                
             </div>
             <div className="w-screen h-screen fixed top-0 left-0 flex justify-center">
                 <div className="flex flex-col justify-center">
-                    <span className="bg-white opacity-100 p-4 rounded fixed">
+                    <span className="bg-white  p-4 rounded fixed">
                         <div className="flex justify-end">
                             <div onClick={onClose} className="cursor-pointer">
                                 <CrossIcon />
